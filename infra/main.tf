59811d8a32745ccc57de7176e113b55557ac57d0
Laxmikant Tamboli <laxmikanttamboli0@gmail.com>
2025-09-19 01:12:35 +0530

    made change in s3 name

--- infra/main.tf
+++ infra/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "app_bucket" {
-  bucket = "devops-kickstart-app-bucket-12345"
+  bucket = "devops-kickstart-app-bucket-11092001"
 
   tags = {
     Name        = "devops-kickstart-app-bucket"
