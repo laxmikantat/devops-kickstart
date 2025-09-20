32a6edb96ca311e682353488b9ef05a4a559b5f2
Laxmikant Tamboli <laxmikanttamboli0@gmail.com>
2025-09-20 20:02:40 +0530

    made chnages in s3 bucket

--- infra/output.tf
+++ infra/output.tf
@@ -3,5 +3,6 @@ output "bucket_name" {
 }
 
 output "ec2_public_ip" {
-  value = aws_instance.devops_ec2.public_ip
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.devops_ec2.public_ip
 }
