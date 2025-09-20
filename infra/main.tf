52c1270b12fb1906d45c756314db02a81d211fd6
Laxmikant Tamboli <laxmikanttamboli0@gmail.com>
2025-09-20 19:31:20 +0530

    made chnages in s3 bucket

--- infra/main.tf
+++ infra/main.tf
@@ -1,19 +1,8 @@
-# Generate a random suffix for uniqueness
-resource "random_id" "suffix" {
-  byte_length = 4
-}
-
-# Create S3 bucket
 resource "aws_s3_bucket" "app_bucket" {
-  bucket = "devops-kickstart-bucket-${random_id.suffix.hex}"
+  bucket = var.s3_bucket_name
 
   tags = {
     Name        = "DevOps-Kickstart-Bucket"
     Environment = "Dev"
   }
-}
-
-# Optional: Output the bucket name
-output "s3_bucket_name" {
-  value = aws_s3_bucket.app_bucket.bucket
 }
--- infra/variable.tf
+++ infra/variable.tf
@@ -7,10 +7,10 @@ variable "ghcr_pat" {
   description = "GitHub Container Registry PAT"
   type        = string
 }
-variable "bucket_prefix" {
-  type    = string
-  default = "devops-kickstart-bucket"
-  description = "Prefix for the S3 bucket name"
+variable "s3_bucket_name" {
+  description = "Name of the S3 bucket"
+  default = "dev-ops-kick-laxmikat:"
+  type        = string
 }
 
 
